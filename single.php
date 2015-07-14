3231f9ee1fc2728c49524595736cabeeaad74275
tcandens <joey.thies@gmail.com>
2015-07-14 15:36:09 -0700

    Working sort of

--- single.php
+++ single.php
@@ -12,10 +12,5 @@
 $context = Timber::get_context();
 $post = Timber::query_post();
 $context['post'] = $post;
-$context['comment_form'] = TimberHelper::get_comment_form();
 
-if ( post_password_required( $post->ID ) ) {
-	Timber::render( 'single-password.twig', $context );
-} else {
-	Timber::render( array( 'single-' . $post->ID . '.twig', 'single-' . $post->post_type . '.twig', 'single.twig' ), $context );
-}
+Timber::render( 'single.twig', $context );
