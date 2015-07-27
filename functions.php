86f7883af05bee24ba64e0fe85f24b9a565cd076
tcandens <joey.thies@gmail.com>
2015-07-27 14:24:58 -0700

    Add <figure> quicktag to wp editor

--- functions.php
+++ functions.php
@@ -60,20 +60,56 @@ function myfoo( $text ) {
   return $text;
 }
 
+/*
+ *Disable the visual editor
+ */
+add_filter('user_can_richedit', create_function( '$a', 'return false;' ), 50);
+
+/*
+ *Add Custom QuickTags to editor
+ */
+function add_quicktags() {
+  if (wp_script_is( 'quicktags' )) {
+?>
+  <script type="text/javascript">
+  QTags.addButton( 'figure', 'figure', '<figure class="window">', '</figure>');
+  </script>
+<?php
+  }
+}
+add_action('admin_print_footer_scripts', 'add_quicktags');
+
+/*
+ *Add editor quicktags on admin
+ */
+function show_quicktags( $qtInit ) {
+  $qtInit['buttons'] = 'strong,em,block,ul,ol,li,link,code,fullscreen';
+  return $qtInit;
+}
+add_filter('quicktags_settings', 'show_quicktags');
+
+/*
+ *Remove Auto Paragraphs from Img tags
+ */
 function filter_ptags_on_images($content){
    return preg_replace('/<p>\s*(<a .*>)?\s*(<img .* \/>)\s*(<\/a>)?\s*<\/p>/iU', '\1\2\3', $content);
 }
 add_filter('the_content', 'filter_ptags_on_images');
 
+/*
+ *Register Site Wide Main Navigation Menu
+ */
 function register_my_menu() {
   register_nav_menu('nav-menu', __( 'Navigation Menu' ));
 }
+add_action( 'init', 'register_my_menu' );
 
+/*
+ *Enqueue Javascript & Stylesheets
+ */
 function enqueue_scripts() {
   wp_enqueue_style( 'stylesheet', get_template_directory_uri() . '/main.css' );
   wp_register_script( 'main-script', get_template_directory_uri() . '/js/main.js', array( 'jquery', 'backbone' ), false, true );
   wp_enqueue_script( 'main-script' );
 }
-
 add_action( 'wp_enqueue_scripts', 'enqueue_scripts' );
-add_action( 'init', 'register_my_menu' );
