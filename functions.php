ea0fc279027afe16699b09ab3dd7b923d3d66081
tcandens <joey.thies@gmail.com>
2015-07-27 11:13:56 -0700

    Add .o-fullwidth-container in components/layout

--- functions.php
+++ functions.php
@@ -61,10 +61,10 @@ function myfoo( $text ) {
 	return $text;
 }
 
-function filter_ptags_on_images($content){
-   return preg_replace('/<p>\s*(<a .*>)?\s*(<img .* \/>)\s*(<\/a>)?\s*<\/p>/iU', '\1\2\3', $content);
-}
-add_filter('the_content', 'filter_ptags_on_images');
+// function filter_ptags_on_images($content){
+//    return preg_replace('/<p>\s*(<a .*>)?\s*(<img .* \/>)\s*(<\/a>)?\s*<\/p>/iU', '\1\2\3', $content);
+// }
+// add_filter('the_content', 'filter_ptags_on_images');
 
 function register_my_menu() {
 	register_nav_menu('nav-menu', __( 'Navigation Menu' ));
