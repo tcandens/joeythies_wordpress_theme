76209664c6aac21877076c4885aec954f373e6de
tcandens <joey.thies@gmail.com>
2015-08-09 20:33:47 -0700

    Make global color variables consistant, tweak color variable applications

--- Gulpfile.js
+++ Gulpfile.js
@@ -11,11 +11,12 @@ var reload = browserSync.reload;
 gulp.task('stylus:dev', function() {
   return gulp.src([ 'stylus/main.styl' ])
     .pipe(sourcemaps.init())
-    .pipe(stylus())
-    .on('error', function(err) {
-      console.log(err + '/n' + 'Stylus Error!');
-      this.emit('end');
-    })
+    .pipe(stylus()
+      .on('error', function(err) {
+        console.log(err + '/n' + 'Stylus Error!');
+        this.emit('end');
+      })
+    )
     .pipe(postcss([ autoprefixer({ browsers: ['last 2 versions'] }) ]))
     .pipe(sourcemaps.write('.'))
     .pipe(gulp.dest('./'))
